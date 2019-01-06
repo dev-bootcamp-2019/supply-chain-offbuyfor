55c723a4c6e83828938d43087180a40a6a563c4c
offbuyfor <43789207+offbuyfor@users.noreply.github.com>
2019-01-06 16:30:39 -0500

    Update SupplyChain.sol

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -9,14 +9,14 @@ pragma solidity ^0.5.0;
 contract SupplyChain {
 
   /* set owner */
-  address owner;
+  address public owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
-
+    uint private skuCount;
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
-
+mapping (uint => Item) public items;
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
     Sold
@@ -24,16 +24,27 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
-
+ enum State {ForSale, Sold, Shipped, Received}
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
   */
-
+ struct Item {
+        string name;
+        uint sku;
+        uint price;
+        State state;
+        address buyer;
+        address seller;
+    }
   /* Create 4 events with the same name as each possible State (see above)
     Each event should accept one argument, the sku*/
-
+    event ForSale(uint sku);
+    event Sold(uint sku);
+    event Shipped(uint sku);
+    event Received(uint sku);
+    
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
 
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
@@ -50,11 +61,25 @@ contract SupplyChain {
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
    that the item with the given sku has the state ForSale. */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale(uint _sku){
+  require(items[_sku].state= State.ForSale);
+    _;
+    }
+  
+  modifier sold(uint _sku) {
+        require(items[_sku].state == State.Sold);
+        _;
+    }
 
+    modifier shipped(uint _sku) {
+        require(items[_sku].state == State.Shipped);
+        _;
+    }
+
+    modifier received(uint _sku) {
+        require(items[_sku].state == State.Received);
+        _;
+    }
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
